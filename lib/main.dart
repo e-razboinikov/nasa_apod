406414492fea9cf541c70b0fa2370eef18d8edfa
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-01-30 16:43:26 +0500

    Provided package removed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 import './providers/apod_provider.dart';
 import 'screens/apod_scaffold_changer.dart';
@@ -14,38 +13,35 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ApodProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'NASA APOD',
-        theme: ThemeData(
-          canvasColor: Colors.black,
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color.fromRGBO(11, 65, 141, 1),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NASA APOD',
+      theme: ThemeData(
+        canvasColor: Colors.black,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color.fromRGBO(11, 65, 141, 1),
+        ),
+        textTheme: TextTheme(
+          headline6: GoogleFonts.spaceGrotesk(
+            fontSize: 24,
+            color: Colors.white,
           ),
-          textTheme: TextTheme(
-            headline6: GoogleFonts.spaceGrotesk(
-              fontSize: 24,
-              color: Colors.white,
-            ),
-            headline5: GoogleFonts.spaceGrotesk(
-              fontSize: 20,
-              color: Colors.white,
-            ),
-            bodyText2: GoogleFonts.spaceGrotesk(
-              wordSpacing: 10.0,
-              fontWeight: FontWeight.w200,
-              fontSize: 18,
-              color: Colors.white,
-            ),
+          headline5: GoogleFonts.spaceGrotesk(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+          bodyText2: GoogleFonts.spaceGrotesk(
+            wordSpacing: 10.0,
+            fontWeight: FontWeight.w200,
+            fontSize: 18,
+            color: Colors.white,
           ),
         ),
-        home: const ApodScreen(),
-        routes: {
-          FullScreenImage.routeName: (_) => const FullScreenImage(),
-        },
       ),
+      home: const ApodScreen(),
+      routes: {
+        FullScreenImage.routeName: (_) => const FullScreenImage(),
+      },
     );
   }
 }
--- lib/screens/apod_content_scaffold.dart
+++ lib/screens/apod_content_scaffold.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import '../providers/apod_provider.dart';
