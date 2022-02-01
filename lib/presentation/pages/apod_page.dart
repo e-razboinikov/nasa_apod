96372e84fcc6501e8b53afdc1ba8127b17478c88
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-02-01 10:50:36 +0500

    Changed BLoC and UI connection.

--- lib/presentation/pages/apod_page.dart
+++ lib/presentation/pages/apod_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-import './loaded_apod_scaffold.dart';
+import './full_screen_image.dart';
 import '../../bloc/apod_bloc.dart';
+import '../widgets/custom_image_loader.dart';
 
 class ApodPage extends StatefulWidget {
   const ApodPage({Key? key}) : super(key: key);
@@ -13,18 +15,17 @@ class ApodPage extends StatefulWidget {
 }
 
 class _ApodPageState extends State<ApodPage> {
-  late ApodBloc bloc;
+  DateTime date = DateTime.now();
+
   void _showDatePicker() {
     showDatePicker(
-      builder: (context, child) {
-        return Theme(
-          data: ThemeData.dark(),
-          child: child!,
-        );
-      },
+      builder: (context, child) => Theme(
+        data: ThemeData.dark(),
+        child: child!,
+      ),
       fieldHintText: 'MM-DD-YYYY',
       context: context,
-      initialDate: bloc.date,
+      initialDate: date,
       firstDate: DateTime.parse('1995-06-16'),
       lastDate: DateTime.now(),
     ).then((value) {
@@ -38,78 +39,89 @@ class _ApodPageState extends State<ApodPage> {
     if (direction == DismissDirection.startToEnd) {
       context.read<ApodBloc>().add(
             LoadApodEvent(
-                date: DateTime(
-              bloc.date.year,
-              bloc.date.month,
-              bloc.date.day - 1,
-            )),
+              date: DateTime(date.year, date.month, date.day - 1),
+            ),
           );
     } else if (direction == DismissDirection.endToStart) {
       context.read<ApodBloc>().add(
             LoadApodEvent(
-                date: DateTime(
-              bloc.date.year,
-              bloc.date.month,
-              bloc.date.day + 1,
-            )),
+              date: DateTime(date.year, date.month, date.day + 1),
+            ),
           );
     }
   }
 
   @override
   void initState() {
-    bloc = context.read<ApodBloc>();
-    context.read<ApodBloc>().add(LoadApodEvent(date: bloc.date));
+    context.read<ApodBloc>().add(LoadApodEvent(date: date));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(DateFormat('EEE, MMMM dd, yyyy').format(bloc.date)),
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Icons.change_circle_outlined,
-            ),
-            onPressed: () =>
-                context.read<ApodBloc>().add(LoadRandomApodEvent()),
-          ),
-          IconButton(
-            onPressed: () => _showDatePicker(),
-            icon: const Icon(
-              Icons.calendar_today_outlined,
-            ),
-          ),
-        ],
-      ),
-      body: Dismissible(
+    return BlocConsumer<ApodBloc, ApodState>(
+      listener: (ctx, state) =>
+          state.maybeMap(loading: (state) => date = state.date, orElse: () {}),
+      builder: (ctx, state) => Scaffold(
+        appBar: AppBar(
+            title: Text(DateFormat('EEE, MMMM dd, yyyy').format(date)),
+            actions: [
+              IconButton(
+                  icon: const Icon(Icons.change_circle_outlined),
+                  onPressed: () => context
+                      .read<ApodBloc>()
+                      .add(const LoadRandomApodEvent())),
+              IconButton(
+                  onPressed: () => _showDatePicker(),
+                  icon: const Icon(Icons.calendar_today_outlined)),
+            ]),
+        body: Dismissible(
           key: ValueKey(DateTime.now()),
-          direction: (bloc.date.day == DateTime.now().day)
+          direction: (date.day == DateTime.now().day)
               ? DismissDirection.startToEnd
               : DismissDirection.horizontal,
           onDismissed: (direction) => _swiping(direction),
-          child: BlocConsumer<ApodBloc, ApodState>(
-            listener: (ctx, state) {},
-            builder: (ctx, state) {
-              if (state is InititalApodState) {
-                return const Center();
-              }
-              if (state is LoadingApodState) {
-                return const Center(
-                    child: CircularProgressIndicator.adaptive());
-              }
-              if (state is LoadedApodState) {
-                return LoadedApodScaffold(
-                    apodData: state.apod, date: bloc.date);
-              }
-              if (state is FailureApodState) {
-                return Center(child: Text(state.errorMessage));
-              }
-              return const Scaffold();
-            },
-          )),
+          child: state.map(
+            initial: (state) => const Center(),
+            loading: (state) =>
+                const Center(child: CircularProgressIndicator.adaptive()),
+            loaded: (state) => SlidingUpPanel(
+              color: Colors.black,
+              collapsed: Container(
+                color: Colors.black,
+                child: Center(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.arrow_upward_outlined, color: Colors.white),
+                        Text('Explanation')
+                      ]),
+                ),
+              ),
+              panel: SingleChildScrollView(
+                child: Text(state.apod.explanation),
+              ),
+              body: Stack(children: [
+                GestureDetector(
+                  onTap: () => MaterialPageRoute(
+                      builder: (ctx) =>
+                          FullScreenImage(imageUrl: state.apod.imageUrl)),
+                  child: CustomImageLoader(imageUrl: state.apod.imageUrl),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  color: Colors.black45,
+                  width: double.infinity,
+                  child: Text(state.apod.title,
+                      style: Theme.of(context).textTheme.headline5,
+                      textAlign: TextAlign.end),
+                ),
+              ]),
+            ),
+            failure: (state) => Center(child: Text(state.errorMessage)),
+          ),
+        ),
+      ),
     );
   }
 }
