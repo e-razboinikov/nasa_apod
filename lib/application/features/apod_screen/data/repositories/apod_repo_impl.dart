cc28b53fe2c133f9ba59f07c89428e49ca12c322
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-02-25 11:23:58 +0500

    Apod repo impl now correct.

--- lib/application/features/apod_screen/data/repositories/apod_repo_impl.dart
+++ lib/application/features/apod_screen/data/repositories/apod_repo_impl.dart
@@ -1,9 +1,12 @@
-import 'package:dio/dio.dart';
-import 'package:nasa_apod/application/features/apod_screen/data/data_sources/api/apod_api.dart';
+import 'package:nasa_apod/application/features/apod_screen/data/data_sources/services/apod_service.dart';
 import 'package:nasa_apod/application/features/apod_screen/data/models/apod.dart';
 import 'package:nasa_apod/application/features/apod_screen/domain/repositories/apod_repo.dart';
 
 class ApodRepoImpl extends ApodRepo {
+  final ApodService apodService;
+
+  ApodRepoImpl({required this.apodService});
+
   Future<Apod> getApod({required String date}) async =>
-      await ApodApi(Dio()).getApod(date: date);
+      await apodService.getApod(date: date);
 }
