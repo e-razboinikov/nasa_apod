0f2b31d895026301793a858983c805d02b70f5a5
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-02-27 11:58:51 +0500

    Added date for APOD entity.

--- lib/features/apod_screen/domain/entities/apod_entity.dart
+++ lib/features/apod_screen/domain/entities/apod_entity.dart
@@ -1,13 +1,22 @@
 import 'package:equatable/equatable.dart';
 
 class ApodEntity extends Equatable {
+  final String date;
   final String title;
   final String explanation;
   final String url;
 
-  const ApodEntity(
-      {required this.title, required this.explanation, required this.url});
+  const ApodEntity({
+    required this.date,
+    required this.title,
+    required this.explanation,
+    required this.url,
+  });
 
   @override
-  List<Object?> get props => [title, explanation, url];
+  List<Object?> get props => [
+        title,
+        explanation,
+        url,
+      ];
 }
