71a28562cd61bd4b6afeca7e845c82726fee1d63
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-04-18 09:24:52 +0500

    Unused import deleted.

--- lib/features/apod_screen/presentation/pages/full_screen_image.dart
+++ lib/features/apod_screen/presentation/pages/full_screen_image.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:nasa_apod/features/apod_screen/presentation/widgets/custom_image_loader.dart';
 
 class FullScreenImage extends StatelessWidget {
